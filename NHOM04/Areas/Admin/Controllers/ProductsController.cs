634f0646862214c396aba94095fe5f0ea722d4c7
Dell <0306201263@caothang.edu.vn>
2022-11-10 15:34:35 +0700

    Areas

---
+++ NHOM04/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NHOM04.Areas.Admin.Controllers
+{
+
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
