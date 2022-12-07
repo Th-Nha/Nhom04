811a1c81596b68ff6316ee150871c046ae93679f
the vinh <0306201300@caothang.edu.vn>
2022-12-07 22:58:51 +0700

    Update Model and View Cart

---
+++ NHOM04/Controllers/CartsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NHOM04.Data;
+using NHOM04.Models;
+
+namespace NHOM04.Controllers
+{
+    public class CartsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
